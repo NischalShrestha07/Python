e417a521cde4732847a67a33615fb870e31b1b00
Nischal <nischalshrestha800@gmail.com>
2024-02-28 23:06:00 +0545

    .

--- practice/doc1.py
+++ practice/doc1.py
@@ -4,6 +4,6 @@ n=5**2
 print("The square of the number is:",n)
 
 
-# The // helps to get the floor value when the output comes in decimal
+# The // helps to get the floor value when the output comes in decimals
 a=18//5
 print(a)
