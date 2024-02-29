7a6b2c2c2e7c1e5cec405eb06992490c2de81b2e
Nischal <nischalshrestha800@gmail.com>
2024-02-29 09:50:46 +0545

    .

---
+++ practice/doc3.py
@@ -0,0 +1,10 @@
+x=int(input("Enter the value of x"))
+match x:
+    case 0:
+        print("x is zero.")
+    case 4:
+        print("x is 4.")
+        
+    case _:
+        print("x is default.")
+        
