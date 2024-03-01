c8738fa7d9298c2522c70c3c7b328d23c8b5e0ab
Nischal <nischalshrestha800@gmail.com>
2024-03-01 08:36:34 +0545

    .

---
+++ practice/doc5.py
@@ -0,0 +1,17 @@
+day=int(input("Enter the day according to numbers(1-7)"))
+if(day==1):
+    print("Today is Sunday")
+elif(day==2):
+    print("Today is Monday")
+elif(day==3):
+    print("Today is Tuesday")
+elif(day==4):
+    print("Today is Wednesday")
+elif(day==5):
+    print("Today is Thrusday")
+elif(day==6):
+    print("Today is Friday")
+elif(day==7):
+    print("Today is Saturday")
+else:
+    print("Invalid Options")
