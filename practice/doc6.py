708f8b74710f5fd26157efa90531a0791436667e
Nischal <nischalshrestha800@gmail.com>
2024-03-01 09:09:21 +0545

    .

---
+++ practice/doc6.py
@@ -0,0 +1,21 @@
+# # words=['cat','tiger','lion','donkey']
+# # for i in words:
+# #     print(i,"The length of the string is:",len(i))
+# word="Banana"
+# for i in word:
+#     print(i)
+# word={"Banana","Apple"}
+# for i in word:
+#     print(i)
+
+
+users={
+    'Name':'Nischal',
+    'LastName':'Shrestha',
+    'Address':'Biratnagar',
+    'Age':'20',
+    'Members':'4'
+}
+for i in users:
+    print(users,"\n")
+    # print("")
