7ff531485a7d69fd3dec8779f83fec8a89c5528f
Nischal <nischalshrestha800@gmail.com>
2024-03-01 09:43:42 +0545

    .

--- practice/doc9.py
+++ practice/doc9.py
@@ -1,8 +1,15 @@
-# for i in range(1,21):
-#     print(i)
+# # for i in range(1,21):
+# #     print(i)
     
-sum=0    
-# for i in range(10,21):
-for i in range(0,6):
-    sum=sum+i
-print("The total sum fo the number from 10 to 20 is:",sum)
+# sum=0    
+# # for i in range(10,21):
+# for i in range(0,6):
+#     sum=sum+i
+# print("The total sum fo the number from 10 to 20 is:",sum)
+
+
+colors=["Red","Blue","Green","Voilet"]
+# for color in colors:
+#     print(color)
+for m in colors:
+    print(m)
