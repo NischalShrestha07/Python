12f0a59b11b7e21abbe05dee04feb463fac79aca
Nischal <nischalshrestha800@gmail.com>
2024-03-02 23:44:30 +0545

    .

--- practice/doc12.py
+++ practice/doc12.py
@@ -1,7 +1,15 @@
-for i in range(12):
-    if(i==10):
+# for i in range(12):
+#     if(i==10):
+#         break
+#     print("5 X",i+1,"=",5*(i+1))
+
+
+# print("Loops bata niskiyo hai")
+
+
+while True:
+    print(i)
+    i=i+1
+    if(i%100==0):
         break
-    print("5 X",i+1,"=",5*(i+1))
-
-
-print("Loops bata niskiyo hai")
+        
