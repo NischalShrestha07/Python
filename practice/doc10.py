e356d6655d8feb665ee84659bd870d81d7b7c17f
Nischal <nischalshrestha800@gmail.com>
2024-03-02 21:53:06 +0545

    .

--- practice/doc10.py
+++ practice/doc10.py
@@ -1,3 +1,10 @@
 name="Nischal"
 for i in name:
     print(i)
+    if(i=='s'):
+        print("The special 's' letter has came. ")
+
+
+colors=['red','blue','green','black']   
+for color in colors:
+    print(color)
