b6e91d644935a2be794aea6d41179ee89154446e
Nischal <nischalshrestha800@gmail.com>
2024-03-02 22:44:53 +0545

    .

--- practice/doc10.py
+++ practice/doc10.py
@@ -1,19 +1,19 @@
-name="Nischal"
-for i in name:
-    print(i)
-    if(i=='s'):
-        print("The special 's' letter has came. ")
+# name="Nischal"
+# for i in name:
+#     print(i)
+#     if(i=='s'):
+#         print("The special 's' letter has came. ")
 
 
-colors=['red','blue','green','black']   
-for color in colors:
-    print("")
+# colors=['red','blue','green','black']   
+# for color in colors:
+#     print("")
     
-    print("=>",color)
-    # print("")
+#     print("=>",color)
+#     # print("")
 
-    for i in color:
-        print(i)    
+#     for i in color:
+#         print(i)    
 
 n=int(input("Enter the number:"))
 print("The number upto ",n ,"are:")
---
+++ practice/doc11.py
@@ -0,0 +1,11 @@
+# i=0
+# n=int(input("Enter the number:"))
+# while(i<=n):
+#     print(i)
+#     i=i+1
+num=0
+
+while(num<30):
+    num=int(input("Enter any number:"))
+    print(num)
+    # print("The greater number is entered.")
