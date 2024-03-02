9e6993e67d9e58c0f918a6656403dab7be017051
Nischal <nischalshrestha800@gmail.com>
2024-03-02 22:53:01 +0545

    .

--- practice/doc11.py
+++ practice/doc11.py
@@ -3,9 +3,20 @@
 # while(i<=n):
 #     print(i)
 #     i=i+1
-num=0
 
-while(num<30):
-    num=int(input("Enter any number:"))
-    print(num)
+# Code yto print upto the entered number:
+
+# num=0
+
+# while(num<30):
+#     num=int(input("Enter any number:"))
+#     print(num)
     # print("The greater number is entered.")
+
+
+# Code yto print upto the entered number: in reverse
+count=int(input("Enter the number:"))
+while(count>=0):
+    print(count)
+    # count=count-1
+    count=count-2
