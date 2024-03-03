1ddd329365d2928d4d83e51d76dad80dac2e199c
Nischal <nischalshrestha800@gmail.com>
2024-03-03 09:02:42 +0545

    .

--- practice/doc13.py
+++ practice/doc13.py
@@ -1,12 +1,24 @@
-def calculateGmean(a,b):
-    mean=(a-b)/(a+b)
-    print(mean)
-a=int(input("Enter first number"))    
-b=int(input("Enter second number"))    
-calculateGmean(a,b)
-c=int(input("Enter value of c:"))    
-d=int(input("Enter value of d:"))    
+# def calculateGmean(a,b):
+#     mean=(a-b)/(a+b)
+#     print(mean)
+# a=int(input("Enter first number"))    
+# b=int(input("Enter second number"))    
+# calculateGmean(a,b)
+# c=int(input("Enter value of c:"))    
+# d=int(input("Enter value of d:"))    
 
 
-gmean2=(c+d)/(c+d)
-print(gmean2)
+# gmean2=(c+d)/(c+d)
+# print(gmean2)
+
+def check(a,b):
+    if(a>b):
+        print(a, "is greater than",b)
+    else:
+        print(b, "is greater than",a)
+
+
+n1=int(input("Enter first number"))    
+n2=int(input("Enter second number"))               
+print("The greater number among two entered number is:")
+check(n1,n2)
