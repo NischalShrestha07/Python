034165034c319ca32103388980d886a6193427a2
Nischal <nischalshrestha800@gmail.com>
2024-03-03 08:54:38 +0545

    .

---
+++ practice/doc13.py
@@ -0,0 +1,12 @@
+def calculateGmean(a,b):
+    mean=(a-b)/(a+b)
+    print(mean)
+a=int(input("Enter first number"))    
+b=int(input("Enter second number"))    
+calculateGmean(a,b)
+c=int(input("Enter value of c:"))    
+d=int(input("Enter value of d:"))    
+
+
+gmean2=(c+d)/(c+d)
+print(gmean2)
