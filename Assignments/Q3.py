c88668971ed4d53400cc7d09d5d2ed11f901aaf7
Nischal <nischalshrestha800@gmail.com>
2024-03-24 22:24:52 +0545

    .

--- Assignments/Q3.py
+++ Assignments/Q3.py
@@ -1,9 +1,9 @@
 # WAP TO FIND THE LARGEST NUMBERS AMONG THE THREE NUMBERS:
 
-
-n1=int(input("Enter any First Number: "))
-n2=int(input("Enter any Second Number: "))
-n3=int(input("Enter any Third Number: "))
+  
+n1=int(input("Enter the First Number: "))
+n2=int(input("Enter the Second Number: "))
+n3=int(input("Enter the Third Number: "))
 
 if(n1>n2 and n1>n3):
     print("The Largest number is:",n1)
