727d43216d79edf0945084b7de92bbb9e5aa23b9
Nischal <nischalshrestha800@gmail.com>
2024-03-24 17:14:12 +0545

    .

---
+++ Assignments/Q10.py
@@ -0,0 +1,23 @@
+# WAP TO CREATE A LIST AND PERFORM THE FOLLOOWING METHODS 1)insert()  2) remove() 3) append()  4) len()  5)pop() 
+
+
+Names=['Ramesh','Manish','Bob-Marly','Umesh','Basanta','Digital']
+print("\n")
+
+Names.insert(2,'Bardali')
+print("The Names details after insert() are:",Names)
+print("\n")
+
+Names.remove('Umesh')
+print("The Names details after remove() are:",Names)
+print("\n")
+
+Names.append('Nischal')
+print("The Names details after append() are:",Names)
+print("\n")
+
+Names.pop(5)
+print("The Names details after pop() are:",Names)
+
+# print(Names)
+
