5874ab775c4dac20e5c9a3625ac3c86f6439cdf0
Nischal <nischalshrestha800@gmail.com>
2024-03-25 16:38:32 +0545

    ..

---
+++ Assignments/Q14.py
@@ -0,0 +1,10 @@
+# WAP TO CREATE A TUPLE AND PERFORM THE FOLLOWING METHODS:
+# 1. Add items
+# 2. len()
+# 3. check for item in tuple 
+# 4. access items
+
+myTuple=(1,2,3,4,5)
+# tuple cannot be changed but can be concadinate two tuples
+newTuple=myTuple+(8,9)
+print("New Tuple after adding items:",newTuple)
