4b7f1a4ed236255006a989e75f004970536aeb36
Nischal <nischalshrestha800@gmail.com>
2024-03-25 17:19:30 +0545

    .

--- Assignments/Q15.py
+++ Assignments/Q15.py
@@ -1,2 +1,7 @@
 # WAP for filter() to filter  only even numbers from a given list.
 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+
+print("Even numbers from the list:", even_numbers)
---
+++ Assignments/Q16.py
@@ -0,0 +1,6 @@
+# WAP for map()  function to double all items in the list.
+numbers=[1,2,3,4,5]
+
+doubleNumbers=list(map(lambda x:x*2,numbers))
+print("Original Numbers:",numbers)
+print("Doubled Numbers:",doubleNumbers)
