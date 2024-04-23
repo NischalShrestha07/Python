20722b5a7f9b14820de12577c1a8a36c870b1c10
Nischal <nischalshrestha800@gmail.com>
2024-04-23 14:57:57 +0545

    different methods

--- day1.1.py
+++ day1.1.py
@@ -3,6 +3,7 @@ print(name)
 name.append("Niraj")
 name.extend("dipan")
 name.insert(0,"Hikmat")
+name.insert(1,"rohan")
 # name.pop("rohan")
 
 del name[0]
@@ -11,4 +12,7 @@ subName=name.copy()
 print(f"These are datas of name: {name}")
 print()
 print(F"These are the datas of subNames:{subName}")
+number=subName.count("rohan")
 
+print("\n")
+print(f"The number of time string repeated is:{number}")
