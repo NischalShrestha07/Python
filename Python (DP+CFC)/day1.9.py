d8ff85de01e4a5d303e78d78de1940c4d743f475
Nischal <nischalshrestha800@gmail.com>
2024-04-25 12:46:45 +0545

    List in Pythons

---
+++ Python (DP+CFC)/day1.9.py
@@ -0,0 +1,15 @@
+
+
+friend={
+    'name':'Nischal',
+    'age':21
+}
+friend["isNepali"]=True
+
+print(f"Your name is: {friend['name']}")
+print(f"Your age is: {friend['age']}")
+
+# Below one is the JS syntax
+# print(`These are your details:`${friend})
+
+print(f"These are your details:\n {friend}");
