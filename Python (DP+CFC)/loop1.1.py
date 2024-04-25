71836e0c695188407400b1314e025eba87cd5a93
Nischal <nischalshrestha800@gmail.com>
2024-04-25 17:28:55 +0545

    Loops Details

---
+++ Python (DP+CFC)/loop1.1.py
@@ -0,0 +1,25 @@
+
+
+fruits=["nischal","krishna","hari","shyam"]
+for hello in fruits:
+    print(hello)
+    print(hello.upper())    
+    
+numbers={1,2,3,4,5,6}    
+for num in numbers:
+    print(num)
+
+
+grades={
+    'Nischal':5,
+    'Naruto':65,
+    'Boruto':95,
+    'Krishna':45
+}
+# print(f"{grades[0]} has scored ")
+for name in grades:
+    print(f"{name} has got {grades.items}")
+
+# print()
+
+        
