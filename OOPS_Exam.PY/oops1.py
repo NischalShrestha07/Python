fec94f89d82ab6f9b56c4832ab881127fe8dac4a
Nischal <nischalshrestha800@gmail.com>
2024-05-24 20:52:09 +0545

    OOPS  Problems

---
+++ OOPS_Exam.PY/oops1.py
@@ -0,0 +1,28 @@
+# Student Example
+class Student:
+    name="kasan"
+    def __init__(self,name):
+        self.name=name
+        # print(self)
+        print("Adding New Student in Database.")
+        
+
+
+
+# class Caste:
+#     cast="Shrestha"
+    
+s1=Student("Nischal")
+# print(s1)
+print(s1.name)
+# s2=Caste()
+# print(s2.cast)
+
+# # Car Example
+# class Car:
+#     color="red"
+#     brand="Mercedes"
+# car1=Car()
+# print(car1.color)
+# print("The color of the car is:",car1.color)
+# print("The brand of the car is:",car1.brand)
