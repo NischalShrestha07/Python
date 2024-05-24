524c362e6feadca4e8a2fb91fef1c628897d11e0
Nischal <nischalshrestha800@gmail.com>
2024-05-24 20:30:07 +0545

    Py Questions Exams

---
+++ OOPS_Exam.PY/timeExecution.py
@@ -0,0 +1,10 @@
+import time
+def fun():
+    start_time=time.time()
+    s=0
+    for i in range(1,n+1):
+        s=s+i
+    end_time=time.time()    
+    return s, end_time-start_time
+n=5
+print(fun())    
