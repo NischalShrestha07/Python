456c54629daaaa6151d898b4817cfe3f290a4b9e
Nischal <nischalshrestha800@gmail.com>
2024-05-25 19:13:49 +0545

    OOPS  Problems

---
+++ OOPS_Exam.PY/methods1.py
@@ -0,0 +1,10 @@
+
+myList=["Nischal","45","4.52",5.65,7825]
+# for i in myList:
+    # print(i)
+    # print(f"Data Type of {i} is:",type(i))
+    # print(myList[::-1])
+# myList.reverse()
+myList.remove("Nischal")
+myList.remove("4.52")
+print(myList)
---
+++ OOPS_Exam.PY/square.py
@@ -0,0 +1,3 @@
+number=[5,4,6,7,9]
+square=[x**2 for x in number]
+print(square)
