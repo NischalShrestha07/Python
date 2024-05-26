e8f645bae02639a92e9219995ed7c21eb6e6fa99
Nischal <nischalshrestha800@gmail.com>
2024-05-26 22:01:19 +0545

    OOPS  Problems

--- OOPS_Exam.PY/functionGoes.py
+++ OOPS_Exam.PY/functionGoes.py
@@ -11,4 +11,9 @@ class BlackAccount:
     def deposit(self,amount):
         self.balance+=amount    
         print(f"Deposited {amount}. New balance is{self.balance}")
-        
+    def withdraw(self,amount):
+        if amount>self.balance:
+            print("Insufficient Funds.")    
+        else:
+            self.balance-=amount    
+            print(f"Deposited {amount}. New balance is {self.balance}.")        
