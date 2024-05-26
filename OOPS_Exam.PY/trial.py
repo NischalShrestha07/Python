0606bafdfd3aa62ece7b25dbd38e1a15e5372d53
Nischal <nischalshrestha800@gmail.com>
2024-05-26 17:59:46 +0545

    OOPS  Problems

---
+++ OOPS_Exam.PY/trial.py
@@ -0,0 +1,22 @@
+
+# Multi Line Strings
+# lang="""
+# ajvdsjjajsabd
+# sjh
+# tje taeb of 
+# """
+# print(lang)
+
+# # Repetations of Strings
+# hel="HAHAHA"*5
+# print(hel)
+
+from math import floor
+
+# getting to least integer
+x=4.5
+print(floor(x))
+
+
+name,age,address="Nischal",21,"Biratnagar"
+print(name,age,address)
