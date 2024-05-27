1347ff7d98b253f5bd23ca300f2ac25ef84cd697
Nischal <nischalshrestha800@gmail.com>
2024-05-27 09:20:03 +0545

    Methods in Dictionary

---
+++ OOPS_Exam.PY/dictionaryFeat1.py
@@ -0,0 +1,56 @@
+
+# Methods to explore:
+# 1. keys()
+# 2. values()
+# 3. items()
+# 4. get()
+# 5. pop()
+# 6. popitem()
+# 7. update()
+# 8. clear()
+
+from email.utils import getaddresses
+from turtle import st
+
+
+student={
+    "name":"Nischal",
+    "age":21,
+    "address":"NEPAL",
+    'grade':{
+        'Math':91,
+        'Science':85,
+        'Nepali':53
+    }
+}
+# only prints keys 
+print(student.keys())
+
+print(student.values())
+print("")
+
+# prints every items present on the student.
+print("All Items:",student.items())
+
+# use of get() method 
+print("Age is:",student.get('age'))
+
+# printing single value from dictionary
+name=student.pop("name")
+print("Popped: ",name)
+
+# below is to access the single value from the dictionary.
+mathGrade=student.pop('grade')["Math"]
+print("Math Grade:",mathGrade)
+
+# use of popitem() which pops the last keys and values.
+getAddress=student.popitem()
+print("Last Item is:",getAddress)
+
+# use of update() method
+student.update({
+    "Name":"Pri N ka",
+    "qualification":"Bachelor",
+    "Faculty":"Computer engineering"
+})
+print("Updated Student Details:\n",student)
--- OOPS_Exam.PY/oops3.py
+++ OOPS_Exam.PY/oops3.py
@@ -1,14 +1,18 @@
-class FlyAble:
-    def fly(self):
-        return "The duck is flying."
-class Swimmable:
-    def swim(self):
-        return "The duck is swimming."        
+# class FlyAble:
+#     def fly(self):
+#         return "The duck is flying."
+# class Swimmable:
+#     def swim(self):
+#         return "The duck is swimming."        
 
-class Duck(FlyAble,Swimmable):
-    pass
+# class Duck(FlyAble,Swimmable):
+#     pass
 
-duck=Duck()        
-print(duck.fly())
-print(duck.swim())
+# duck=Duck()        
+# print(duck.fly())
+# print(duck.swim())
+    
+name = "Alice"
+age = 30
+print("Name: %s, Age: %d" % (name, age))
     
